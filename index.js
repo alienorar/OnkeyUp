658e9ba8541e577d00b7bb6cfc4c91360d9dde52
alienorar <rayhonakhayrullaeva@gmail.com>
2024-08-02 01:53:18 +0500

    task 5 added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const count = document.getElementById("count")
 function countText(event) {
   event.preventDefault()
   let el = event.target.value
-  count.innerHTML = el.split(" ").map((item, index) => (item !== "" && item !== ",", index++)).length + "words you entered"
+  count.innerHTML = el.split(" ").map((item, index) => (item !== "", index++)).length + "words you entered"
 
 }
 
@@ -55,9 +55,38 @@ create.addEventListener("click", function createElement(e) {
 
 // task 4
 
+const fileInput = document.getElementById("file")
+const volumeOutput = document.getElementById("volume")
+function checkVolume() {
+  if (fileInput.files.length > 0) {
+    const file = fileInput.files[0]
+    const fileSizeInKb = (file.size / 1024).toFixed(2)
+    volumeOutput.innerText = `Size:  ${fileSizeInKb} Kb`
+    console.log((file.size / 1024).toFixed(2));
+  }
+  else {
+    volumeOutput.innerText = ""
+  }
+}
 
+// Task 5
 
+const bracesInput = document.getElementById("braceInput").value
+const bracesOutput = document.getElementById("bracesOutput")
 
+function checkBraces() {
+  const bracesInput = document.getElementById("braceInput").value
+let openBracket = bracesInput.split("").filter(item => item === ")")
+let closeBracket = bracesInput.split("").filter(item => item ==="(")
+if (openBracket.length === closeBracket.length) {
+  bracesOutput.innerText = "Opening and Closing brackets are equal"
+  bracesOutput.style.color = "green"
+}
+else{
+  bracesOutput.innerText = "Opening and Closing brackets aren't equal"
+  bracesOutput.style.color = "red"
+}
+}
 
 
 
